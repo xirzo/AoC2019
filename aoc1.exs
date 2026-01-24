5898c59d4146db2ac4eb98bf823ed62c11d19688
xirzo <easttem@gmail.com>
2026-01-25 02:03:05 +0300

    init: initial commit

---
+++ aoc1.exs
@@ -0,0 +1,3 @@
+# Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+
+IO.puts("Hello, World")
