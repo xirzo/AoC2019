3dfccf4e398a1e3eec8646423ad6bc38deb5ef93
xirzo <easttem@gmail.com>
2026-01-25 12:49:20 +0300

    feat: add first task solution

--- aoc1.exs
+++ aoc1.exs
@@ -1,3 +1,40 @@
-# Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+# Fuel required to launch a given module is based on its mass.
+# Specifically, to find the fuel required for a module, 
+# take its mass, divide by three, round down, and subtract 2.
 
-IO.puts("Hello, World")
+defmodule Aoc do
+
+defp accumulate_fuel_from_file(fd, acc) do
+  case IO.read(fd, :line)  do
+    :eof -> 
+      File.close fd
+      acc
+    {:error, reason} -> 
+      IO.puts :stderr, reason
+      :error
+    line -> 
+      # IO.puts String.to_integer String.trim line
+      # same  as:
+      # line |> String.trim |> String.to_integer |> IO.puts
+      
+      mass = line |> String.trim |> String.to_integer
+      fuel = div(mass, 3) - 2
+      accumulate_fuel_from_file fd, acc + fuel
+  end
+end
+
+def process_file(filename) do
+  case File.open filename, [:read, :utf8] do
+    {:ok, fd} -> 
+      case accumulate_fuel_from_file fd, 0 do
+        fuel ->
+          IO.puts fuel
+      end
+    {:error, reason} -> 
+      IO.puts :stderr, reason
+  end
+end
+
+end
+
+Aoc.process_file "input1.txt"
